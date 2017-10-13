18fb1d731021d4a312db8be826f7895ccdf029d3
tianya <liuzaisen@heqiauto.com>
2017-10-13 10:12:47 +0800

    Version upgrade

--- src/Exception.php
+++ src/Exception.php
@@ -5,7 +5,7 @@
  * @copyright 2017. Heqiauto Inc.
  * @license   https://opensource.org/licenses/Apache-2.0
  * @link      https://github.com/Heqiauto/hepc-sdk
- * @version   1.2.4
+ * @version   1.2.5
  */
 
 namespace Heqiauto\HepcSdk;

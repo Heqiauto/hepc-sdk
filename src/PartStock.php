abe1272313bb43c9b46d252959d628aaeb82056e
tianya <liuzaisen@heqiauto.com>
2017-10-14 17:45:06 +0800

    Version upgrade

--- src/PartStock.php
+++ src/PartStock.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Hepc SDK - Sdk client for Heqiauto-epc service
+ *
+ * @copyright 2017. Heqiauto Inc.
+ * @license   https://opensource.org/licenses/Apache-2.0
+ * @link      https://github.com/Heqiauto/hepc-sdk
+ */
 
 namespace Heqiauto\HepcSdk;
 
